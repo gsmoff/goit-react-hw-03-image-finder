b96f5846d4bd6beb73f572aa57d865e1c20b3a05
gsmoff <maslovsg@ukr.net>
2023-02-10 15:09:05 +0200

    update state loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,55 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
+import { getImages } from '../services/getImages';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Toaster } from 'react-hot-toast';
 import { Modal } from './Modal/Modal';
 
 export class App extends Component {
     state = {
+        images: [],
         searchText: '',
         currentImg: null,
+        error: '',
+        status: 'idle',
+        page: 1,
+        visibleButton: true,
     };
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.searchText !== this.state.searchText) {
+            this.setState({
+                status: 'pending',
+                visibleButton: true,
+            });
+        }
+        if (prevState.searchText !== this.state.searchText ||
+            prevState.page !== this.state.page
+        ) {
+            getImages(this.state.searchText, this.state.page)
+                .then(response => response.json())
+                .then(data => {
+                    // console.log(data);
+                    if (!data.total) {
+                        // Promise.reject(new Error());
+                        throw new Error();
+                    }
+                    if (data.hits.length < 12) {
+                        this.setState({ visibleButton: false });
+                    }
+                    this.setState(prevState => ({
+                        images: [...prevState.images, ...data.hits],
+                        status: 'resolved',
+                    }));
+                })
+                .catch(error => this.setState({ error, status: 'rejected' }));
+        }
+    }
     handleSubmit = searchText => {
         if (searchText === this.state.searchText) {
             return;
         }
-        this.setState({ searchText });
+        this.setState({ searchText, images: [], page: 1 });
     };
     showMore = data => {
         this.setState({ currentImg: data });
@@ -22,14 +57,30 @@ export class App extends Component {
     closeModal = () => {
         this.setState({ currentImg: null });
     };
+    loadMore = () => {
+        this.setState(prevState => ({ page: prevState.page + 1 }));
+    };
 
-    render() { 
-        const { currentImg, searchText } = this.state;
+    render() {
+        const {
+            currentImg,
+            searchText,
+            images,
+            status,
+            visibleButton,
+        } = this.state;
         return (
             <div>
                 <Toaster position="top-right" toastOption={{ duration: 500 }} />
                 <Searchbar onSearch={this.handleSubmit} />
-                <ImageGallery value={searchText} showImg={this.showMore} />
+                <ImageGallery
+                    value={searchText}
+                    showImg={this.showMore}
+                    images={images}
+                    status={status}
+                    visibleButton={visibleButton}
+                    loadMore={this.loadMore}
+                />
                 {currentImg && (
                     <Modal
                         currentImg={currentImg}
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,60 +1,14 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getImages } from '../../services/getImages';
 import { Loader } from '../Loader/Loader';
 import { ImageGalleryItem } from './ImageGalleryItem';
 import { Button } from '../Button/Button';
 import css from './ImageGallery.module.css';
 
 export class ImageGallery extends Component {
-    state = {
-        images: [],
-        loading: false,
-        error: '',
-        status: 'idle',
-        page: 1,
-        visibleButton: true,
-    };
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.value !== this.props.value) {
-            this.setState({
-                images: [],
-                page: 1,
-                status: 'pending',
-                visibleButton: true,
-            });
-        }
-        if (
-            prevProps.value !== this.props.value ||
-            prevState.page !== this.state.page
-        ) {
-            getImages(this.props.value, this.state.page)
-                .then(response => response.json())
-                .then(data => {
-                    // console.log(data);
-                    if (!data.total) {
-                        // Promise.reject(new Error());
-                        throw new Error();
-                    }
-                    if (data.hits.length < 12) {
-                        this.setState({ visibleButton: false });
-                    }
-                    this.setState(prevState => ({
-                        images: [...prevState.images, ...data.hits],
-                        status: 'resolved',
-                    }));
-                })
-                .catch(error => this.setState({ error, status: 'rejected' }));
-        }
-    }
-    loadMore = () => {
-        this.setState(prevState => ({ page: prevState.page + 1 }));
-    };
 
     render() {
-        const { images, status, visibleButton } = this.state;
-        const { showImg } = this.props;
+        const { showImg, images, status, visibleButton, loadMore } = this.props;
         if (status === 'rejected') {
             return (
                 <h2
@@ -95,7 +49,7 @@ export class ImageGallery extends Component {
                             )}
                     </ul>
                     {images.length !== 0 && visibleButton === true && (
-                        <Button clickHandler={this.loadMore} text="Load More">
+                        <Button clickHandler={loadMore} text="Load More">
                             Load More
                         </Button>
                     )}
